101246c897a0636aee130ffb45cf68c637ac5e71
Ethan Flute <ethansamf@gmail.com>
2022-07-09 21:15:48 -0500

    Fixed small issue.

--- src/test-components/test.js
+++ src/test-components/test.js
@@ -3,19 +3,11 @@ promise = axios.get('https://www.google.com/search?q=test');
 
 promise.then(function(response){
   htmlSource = response.data
-  getLinksFromHtml(htmlSource);
+  linksFromHtml(htmlSource);
 });
 
-function getLinksFromHtml(htmlString) {
-  // Regular expression that matches syntax for a link (https://stackoverflow.com/a/3809435/117030):
+function linksFromHtml(htmlString) {
   LINK_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
-
-  // Use the regular expression from above to find all the links:
   matches = htmlString.match(LINK_REGEX);
-
-  // Output to console:
-  console.log(matches);
-
-  // Alternatively, return the array of links for further processing:
   return matches;
 }
