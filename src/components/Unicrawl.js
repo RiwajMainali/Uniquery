9cbd7fdadbd6e3f62e1357926acfa286d6f569f0
Ethan Flute <ethansamf@gmail.com>
2022-07-11 15:21:08 -0500

    Refactored test.js into Unicrawl, added some functionality and preparing the class for usage in application

--- src/components/Unicrawl.js
+++ src/components/Unicrawl.js
@@ -1,5 +1,7 @@
+// Get Dependencies //
 axios = require('axios');
 
+// TEST INPUT //
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
@@ -16,22 +18,41 @@ readline.question('Enter search query : ', input => {
 
   readline.close();
 });
+// REMOVE ON RELEASE //
 
+function scrapeEngine(engine, input, customFilter) {
+    promise = axios.get('https://www.google.com/search?q=$' + input);
 
-function linksFromHtml(htmlString) {
+    promise.then(function(response){
+      htmlSource = response.data
+      linksFromHtml(htmlSource);
+    });
+
+    return linksFromHtml(htmlString);
+}
+
+// This function will return a table of strings that contain URLs found by a search. //
+function linksFromHtml(htmlString, customFilter) {
+
+  // Regex Configuration //
   LINK_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
   matches = htmlString.match(LINK_REGEX);
   
+  // Filter Configuration //
   searchArray = ['google']
+  searchArray = searchArray.concat(customFilter)
 
+  // Filter Results //
   const texts = matches
     .filter(element => searchArray.every(
       substr => !element.includes(substr)
   ));
 
+  // TEST OUTPUT //
   for (const text of texts) {
     console.log(text);
   }
+  // REMOVE ON RELEASE //
 
   return matches;
 }
