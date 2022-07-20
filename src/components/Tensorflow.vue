f19ea48c43ef60c1cdf08756292362bdac29abf4
Oriana <orianajb18@outlook.com>
2022-07-20 12:25:50 -0500

    fixed cocoSsd issue

--- src/components/Tensorflow.vue
+++ src/components/Tensorflow.vue
@@ -1,8 +1,12 @@
 <script>
 import { ref } from 'vue'
-//require('@tensorflow/tfjs-backend-cpu')
-//require('@tensorflow/tfjs-backend-webgl')
+import '@tensorflow/tfjs-backend-cpu'
+import '@tensorflow/tfjs-backend-webgl'
+//import {default as coco} from  '/node_modules/tensorflow-models/coco-ssd/dist/coco-ssd.js'
+//import '@tensorflow-models/coco-ssd'
 //const cocoSsd = require('@tensorflow-models/coco-ssd')
+//import { cocoSssd } from '@tensorflow-models/coco-ssd'
+import * as cocoSsd from "@tensorflow-models/coco-ssd"
 
 export default {
 	setup(){
