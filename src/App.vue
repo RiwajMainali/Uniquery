571e934b3224fc1453fc65101dc9cb30ca53e2d5
kasiyo <kaiasiripanyo@gmail.com>
2022-07-21 11:37:47 -0500

    fix something i broke #11 (take 2)

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import DarkMode from '@/components/DarkMode.vue'
 import * as axios from 'axios'
 import * as cors from 'cors'
 
-cors({ origin: ['http://localhost:1'],})
+//cors({ origin: ['http://localhost:1'],})
 
 
 export default {
@@ -31,7 +31,6 @@ export default {
             <UniquerySubtext />
             <DarkMode />
         
-            
         </div>
         
     </header>
