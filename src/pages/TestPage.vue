1261dbfaaa0ee69ae522e74f8913ec5aff20a6c9
kasiyo <kaiasiripanyo@gmail.com>
2022-07-21 06:06:38 -0500

    a couple ui changes and test functions
    added new background, realigned some things, testing some things on the test page.

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 </script>
 <script>
+import { onKeyDown } from '@vueuse/core';
 import { RouterLink } from 'vue-router';
 export default {
     methods: {
@@ -9,16 +10,45 @@ export default {
         },
         seeResults() {
             this.$router.push('/results')
-        }
+        },
+        testFunction() {
+            window.location.assign('https://project-61dcd.web.app/searchresults')
+        },
+        getInput() {
+            var userQuery = document.getElementById("query");
+            return userQuery;
+        },
     }
 };
 </script>
 
 <template>
-    This is the test page's body content.
+    <div class="testpage">
         <h1>This is the test page's header</h1>
-
-    <button v-on:click="redirectToPage()">click this to test</button>
-    <button v-on:click="seeResults()">click to see results</button>
     
+
+    
+        This is the test page's body content.
+        <br/><br/><br/>
+        
+        <button v-on:click="redirectToPage()">click this to test</button>
+        <br/><br/><br/>
+        
+        <button v-on:click="seeResults()">click to see results</button>
+        <br/><br/><br/>
+        
+        <input type="text" id="query" autofocus v-on:keyup.enter="getInput()">
+        <p id="userQuery"></p>
+    </div>
+
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .testpage {
+    min-height: 100vh;
+    /*display: flex;*/
+    align-items: center;
+  }
+}
+</style>
