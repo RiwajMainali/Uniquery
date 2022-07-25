8a13f0c1eac056d5bbf0489785786ed18faf53ff
Ethan Flute <ethansamf@gmail.com>
2022-07-25 13:12:48 -0500

    Added support for page-specific queries on all engines, Yandex is slow but functional.

--- src/unicrawl.mjs
+++ src/unicrawl.mjs
@@ -10,15 +10,16 @@ const new_readline = readline.createInterface({
 });
 
 new_readline.question('Enter search query : ', input => {
-  ScrapeEngines(input);
+  ScapeEnginesAtPage(input, 10);
 });
 // REMOVE ON RELEASE //
 
-function GetGoogleResults(input) {
+function GetGoogleResults(input, page_number = 1) {
   // Scrape Google for results
   // https://www.google.com/search?q=$pizza will get page 1.
   // https://www.google.com/search?q=pizza&start=10 will get page 2, https://www.google.com/search?q=pizza&start=20 will get page 3.
-  axios.get('https://www.google.com/search?q=$' + input, {
+
+  axios.get('https://www.google.com/search?q=$' + input + "&start=" + (10 * (page_number - 1)), {
     headers:{}
   }).then((response) => {
     LinksFromHtml(response.data);
@@ -29,11 +30,12 @@ function GetGoogleResults(input) {
   return 0;
 }
 
-function GetBingResults(input) {
+function GetBingResults(input, page_number = 1) {
   // Scrape Bing for results.
   // https://www.bing.com/search?q=pizza
   // https://www.bing.com/search?q=test&first=11 will get page 2, https://www.bing.com/search?q=test&first=21 will get page 3.
-  axios.get('https://www.bing.com/search?q=' + input, {
+
+  axios.get('https://www.bing.com/search?q=' + input + '&first=' + (10 * (page_number - 1) + 1), {
     headers:{}
   }).then((response) => {
     LinksFromHtml(response.data);
@@ -42,14 +44,14 @@ function GetBingResults(input) {
   });
 
   return 0;
-
 }
 
-function GetYandexResults(input) {
+function GetYandexResults(input, page_number = 1) {
   // Scrape Yandex for results.
   // https://yandex.com/search/?text=test&lr=109990&p=0
   // https://yandex.com/search/?text=test&lr=109990&p=1 will get page 2, https://yandex.com/search/?text=test&lr=109990&p=2 will get page 3.
-  axios.get('https://yandex.com/search/?text=' + input + 'test&lr=109990&p=0', {
+
+  axios.get('https://yandex.com/search/?text=' + input + 'test&lr=109990&p=' + (page_number - 1), {
     headers:{}
   }).then((response) => {
     LinksFromHtml(response.data);
@@ -58,31 +60,31 @@ function GetYandexResults(input) {
   });
 
   return 0;
-
-
 }
 
-export function ScrapeEngines(input) {
+export function ScapeEnginesAtPage(input, page_number = 1) {
+    // Ensure correct input (Returns an error-code)
+    if(!isString(input)){
+      return 0;
+    }
+    if(input == ''){
+      return 0;
+    }
+    if(page_number < 1){
+      return -1;
+    }
+    if(page_number > 99){
+      return -2
+    }
 
-  // Ensure correct input
-  if(!isString(input)){
-    return 0;
-  }
-  if(input == ''){
-    return 0;
-  }
+    let results_table = []
 
-  let results_table = []
+    // Create table of results (Temporary, will be more robust in the future for getting stats.)
+    results_table = results_table.concat(GetGoogleResults(input, page_number))
+    results_table = results_table.concat(GetBingResults(input, page_number))
+    results_table = results_table.concat(GetYandexResults(input, page_number))
 
-  // Create table of results (Temporary, will be more robust in the future for getting stats.)
-  results_table = results_table.concat(GetGoogleResults(input))
-  results_table = results_table.concat(GetBingResults(input))
-  results_table = results_table.concat(GetYandexResults(input))
-
-  // Process data and get results.
-
-  // Return table of results from all searches.
-  return results_table;
+    return results_table;
 }
 
 // This function will return a table of strings that contain URLs found by a search. //
@@ -93,7 +95,7 @@ export function LinksFromHtml(htmlString, customFilter) {
   let matches = htmlString.match(LINK_REGEX);
 
   // Filter Configuration //
-  let searchArray = ['google', 'yandex', 'bing']
+  let searchArray = ['google', 'yandex', 'bing', 'w3', 'yastatic']
   searchArray = searchArray.concat(customFilter)
   
   // Filter Results //
