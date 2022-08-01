aad6c06cefcdf1b0cc18e21e319edba4b4c9b42b
Oriana <orianajb18@outlook.com>
2022-08-01 15:40:32 -0500

    removing fluff#

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -2,7 +2,6 @@
 import TheSearchBar from '@/components/TheSearchBar.vue'
 import Tensorflow from '@/components/Tensorflow.vue'
 import { Accessibility } from '/Users/oriana/Uniquery/node_modules/accessibility/src/main'
-//import * as Accessibility from 'accessibility/src/main'
 
 window.addEventListener('load', function() {
     new Accessibility();
