e05307fea59565ff87ee932fae0648da02e07c23
Ethan Flute <ethansamf@gmail.com>
2022-08-03 13:46:06 -0500

    Adding a different CORS fix.

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,10 @@
 const functions = require("firebase-functions");
-const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
  exports.helloWorld = functions.https.onRequest((request, response) => {
-   cors(request, response, () => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-   })
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.set('Access-Control-Allow-Origin', '*');
+  response.send("Hello from Firebase!");
 });
