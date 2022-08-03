fcb28b78c350e9ba2336c8ba3da58ee6e213ffcd
Ethan Flute <ethansamf@gmail.com>
2022-08-03 13:33:57 -0500

    Adding CORS headers to the Firebase clouds functions, this is a test.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require("firebase-functions");
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
  exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
+   cors(request, response, () => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+   })
 });
