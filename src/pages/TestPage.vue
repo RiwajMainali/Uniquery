a70f7e01d2040a07abc08f744eb42c32b06b1c74
Ethan Flute <ethansamf@gmail.com>
2022-08-05 02:24:33 -0500

    JSON fix.

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -9,6 +9,7 @@ import { applyActivation } from '@tensorflow/tfjs-core/dist/ops/fused_util';
 import queryInput from '@/components/queryInput.vue'
 import { useCookies } from 'vue3-cookies';
 import { functions } from '../router/index.js' // this is literally not a valid error, typescript is wrong.
+import { app } from '../router/index.js'
 import { httpsCallable } from 'firebase/functions';
 
 export default {
