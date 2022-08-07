ad8a58c739618aa177059e6fe43f8c191a95f832
Ethan Flute <ethansamf@gmail.com>
2022-08-07 15:51:36 -0500

    Fixing deployment issue with firebase.

--- functions/index.js
+++ functions/index.js
@@ -8,10 +8,12 @@ const functions = require("firebase-functions");
 
   if (req.body.message === undefined) {
     res.status(400).send({error: 'No message defined!'});
+    return res.status(200).json({});
   } 
   else {
     console.log(req.body.message);
     res.status(200).send({data: 'Success: ' + req.body.message});
+    return res.status(200).json({});
   }
   
   //return response.status(200).json({status : "success"});
