d73d8f799cea843d0c4c34b2a5651751014d1182
Ethan Flute <ethansamf@gmail.com>
2022-08-09 16:53:39 -0500

    Adding webcrawler connection to firebase function.

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,28 @@ const cors = require('cors')({ origin: true });
  exports.test = functions.https.onCall((data, context) => {
   //const output = unicrawl.ScrapeEnginesAtPage("Test")
 
-  return axios.get('https://www.google.com/search?q=$test&start=0')
+  axios.get('https://www.bing.com/search?q=pizza')
   .then((response) => {
     console.log("Test 1")
+    // Regex Configuration //
+    let LINK_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+    let matches = response.data.match(LINK_REGEX);
+
+    // Filter Configuration //
+    let searchArray = ['google', 'yandex', 'bing', 'w3', 'yastatic']
+    searchArray = searchArray.concat(customFilter)
+
+    // Filter Results //
+    const texts = matches
+      .filter(element => searchArray.every(
+        substr => !element.includes(substr)
+    ));
+
+    return texts;
   }).catch((error) => {
     console.log("Test 2")
   });
+
+  return "Loss"
   //return response.status(200).json({status : "success"});
 });
