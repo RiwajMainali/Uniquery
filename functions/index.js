bd8dd003d927388037e0674817c4267ce7f3dc82
Ethan Flute <ethansamf@gmail.com>
2022-08-09 15:55:58 -0500

    Changing Axios functionality.

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,23 @@
 const functions = require("firebase-functions");
 //const unicrawl = require('../src/unicrawl.js');
 const axios = require("axios")
+const cors = require('cors')({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
  exports.test = functions.https.onCall((data, context) => {
   //const output = unicrawl.ScrapeEnginesAtPage("Test")
-  axios.get('https://www.google.com/search?q=$' + data + "&start=" + (10 * (page_number - 1)), {
-    headers:{}
-  }).then((response) => {
-    // Regex Configuration //
-    let LINK_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
-    let matches = response.match(LINK_REGEX);
-
-    // Filter Configuration //
-    let searchArray = ['google', 'yandex', 'bing', 'w3', 'yastatic']
-    searchArray = searchArray.concat(customFilter)
-
-    // Filter Results //
-    const texts = matches
-      .filter(element => searchArray.every(
-        substr => !element.includes(substr)
-    ));
-
-    return texts;
+  axios.get('https://www.google.com/search?q=$test&start=0')
+  .then((response) => {
+    console.log("Test 1")
+    return "Success";
   }).catch((error) => {
-    console.log(error)
+    console.log("Test 2")
+    return error;
   });
   
 
-  return "Hello";
+  return "This should not be possible";
   //return response.status(200).json({status : "success"});
 });
